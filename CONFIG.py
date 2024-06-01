47826a3fa4860793a29a8638307b4deedc034f13
LINLINLIN <jiong-lin@live.com>
2024-06-01 11:04:00 +0800

    first commit

--- CONFIG.py
+++ CONFIG.py
@@ -3,7 +3,7 @@ import os
 from pymongo import MongoClient
 
 # 创建一个 MongoClient 对象
-client = MongoClient(os.getenv('MONGODB_URI'), )
+client = MongoClient(os.getenv('MONGODB_URI'))
 
 # 获取数据库，如果数据库不存在，那么它将会被创建
 db = client['u148']
